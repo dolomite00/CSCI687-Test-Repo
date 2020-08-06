c085b0ea5e86ca43d259bbe9f43bb7651cc8a318
Joon S. Lee <dolomite00@gmail.com>
2020-08-06 03:47:58 -0400

    fixed Hello.py and added hw01-01.py

---
+++ HW01/hw01-01.py
@@ -0,0 +1,3 @@
+edge = (int)(input("Enter the cube's edge: "))
+surface_area = (edge ** 2) * 6
+print("The surface area is ", surface_area, " square units.")
--- Hello.py
+++ Hello.py
@@ -1,6 +1,12 @@
-print("Hello World!")
-print("Adding one more print statement")
-print("adding two more lines - 1")
-print("adding two more lines -2")
-print(3 + 4)
+"""
+Program: Hello.py
+Author: Joon S. Lee
+Last Date Modified: 8/6/2020
+
+The purpose of this program is to demonstrate simple print statements.
+"""
+print("Hello CSCI687")  # simple print statement
+print("Let's do some math")  # using ' within " "
+print("47 + 98 is ", 47 + 98)  # you can also do this
+print("""Triple-quoted strings can include "" and '' within the body""")
 
